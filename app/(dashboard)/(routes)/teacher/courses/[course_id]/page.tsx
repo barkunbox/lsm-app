ec0a40e5012d02f90559c57b78e3cea988e2660b
Barkun <barkun.al.fattaah@gmail.com>
2025-08-25 05:50:48 +0700

    price form with drama and inteliphense error many many

--- app/(dashboard)/(routes)/teacher/courses/[course_id]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[course_id]/page.tsx
@@ -3,11 +3,12 @@ import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { IconBadge } from '@/components/icon-badge';
-import { LayoutDashboard } from 'lucide-react';
+import { CircleDollarSign, LayoutDashboard, ListChecks } from 'lucide-react';
 import { TitleForm } from './_components/title-form';
 import { DescriptionForm } from './_components/description-form';
 import { ImageForm } from './_components/image-form';
 import { CategoryForm } from './_components/category-form';
+import { PriceForm } from './_components/price-form';
 
 interface CourseIdPageProps {
   params: Promise<{ course_id: string }>; // 👈 must be a Promise
@@ -93,6 +94,31 @@ const CourseIdPage = async ({
               value: category.id,
             }))}
           />
+        </div>
+        <div className='space-y-6'>
+          <div>
+            <div className='flex items-center gap-x-2'>
+              <IconBadge icon={ListChecks} />
+              <h2 className='text-xl'>
+                Course chapters
+              </h2>
+            </div>
+            <div>
+              TO DO: Chapters
+            </div>            
+          </div>
+          <div>
+            <div className='flex items-center gap-x-2'>
+              <IconBadge icon={CircleDollarSign} />
+              <h2 className='text-xl'>
+                Sell your course
+              </h2>
+            </div>
+            <PriceForm
+              initialData={course}
+              courseId={course.id}
+            />
+          </div>
         </div>
       </div>
     </div>

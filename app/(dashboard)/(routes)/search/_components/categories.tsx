6d301eb770781b37bffd8161c75766cf72095fc1
Barkun <barkun.al.fattaah@gmail.com>
2025-08-26 13:35:49 +0700

    search input and functionality

---
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { categories } from "@prisma/client";
+import {
+  FcEngineering,
+  FcFilmReel,
+  FcMultipleDevices,
+  FcMusic,
+  FcOldTimeCamera,
+  FcSalesPerformance,
+  FcSportsMode,
+} from "react-icons/fc";
+import { IconType } from "react-icons/lib";
+import { CategoryItem } from "./category-item";
+
+interface CategoriesProps {
+  items: categories[];
+}
+
+const iconMap: Record<categories["name"], IconType> = {
+  "Music": FcMusic,
+  "Photography": FcOldTimeCamera,
+  "Fitness": FcSportsMode,
+  "Accounting": FcSalesPerformance,
+  "Computer Science": FcMultipleDevices,
+  "Filming": FcFilmReel,
+  "Engineering": FcEngineering,
+}
+
+export const Categories = ({
+  items,
+}: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  )
+}
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,33 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursePage = () => {
+const CoursePage = async () => {
+
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.courses.findMany({
+    where: {
+      user_id: userId,
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
+      {/* <Link href="/teacher/create">
         <Button>
           New Course
         </Button>      
-      </Link>
+      </Link> */}
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 }
